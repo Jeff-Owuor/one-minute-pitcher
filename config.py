17c37e26f80331c01e46faa795c9e400bbf15217
Jeff <Xavierjeff451@gmail.com>
2022-05-08 22:53:24 +0300

    uploading an image

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config:
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://paulineapondi:1989@localhost/pitched'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
     
 class ProdConfig(Config):
     '''
