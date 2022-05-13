e0aa1e9a8b8573de5e7257f5691a0d6d27dada44
Jeff <Xavierjeff451@gmail.com>
2022-05-13 12:53:25 +0300

    commit

--- app/models.py
+++ app/models.py
@@ -66,7 +66,7 @@ class Pitch(db.Model):
 class Comment(db.Model):
     __tablename__ = 'comments'
     id = db.Column(db.Integer,primary_key=True)
-    
+    comment = db.Column(db.Text(),nullable = False)
     user_id = db.Column(db.Integer,db.ForeignKey('users.id'),nullable = False)
     pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
     
