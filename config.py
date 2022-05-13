7520d954b4a9e6626ef564610aa78f89cdffa72b
Jeff <Xavierjeff451@gmail.com>
2022-05-13 19:11:54 +0300

    commit

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI ='postgres://kkfvxdzbctlrnm:52d984f3b28623ae4b928e62e3823ee693b07fc2dff8b220e12d4f8e0064726b@ec2-34-236-94-53.compute-1.amazonaws.com:5432/d77ca7vm9hn7ou'
+    SQLALCHEMY_DATABASE_URI ='postgresql://kkfvxdzbctlrnm:52d984f3b28623ae4b928e62e3823ee693b07fc2dff8b220e12d4f8e0064726b@ec2-34-236-94-53.compute-1.amazonaws.com:5432/d77ca7vm9hn7ou'
 
 class DevConfig(Config):
     '''
